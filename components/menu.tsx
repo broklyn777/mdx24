7b1f18fcaa9ac0253a022cd9a2cc2aec4639287f
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2024-10-12 00:19:50 +0200

    ändrade cta boka

--- components/menu.tsx
+++ components/menu.tsx
@@ -121,7 +121,7 @@ export default function Menu() {
         {/* Call to Action */}
         <div className="hidden md:block">
           <Link
-            href="/boka"
+            href="/kontakt"
             className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600 transition-all"
           >
             Boka Nu!
